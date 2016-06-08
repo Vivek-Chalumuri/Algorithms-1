e214436c3bebc686e5108d74b495f7f679e61a87
vivek9260 <vivek9260@users.noreply.github.com>
2016-06-08 14:34:19 +0530

    Add files via upload

---
+++ Karatsuba_Multiplication.py
@@ -0,0 +1,24 @@
+from math import *
+
+def Karatsuba_Mult(X,Y):
+	if X < 10 or Y < 10:
+		return X*Y
+	
+	Base = 10
+	
+	n = max(len(str(X)),len(str(Y)))
+	
+	if n % 2 != 0:
+		n -= 1
+	
+	m = n/2
+	
+	a , b = divmod(X,Base**m)
+	c , d = divmod(Y,Base**m)
+	
+	ac = Karatsuba_Mult(a,c)
+	bd = Karatsuba_Mult(b,d)
+	k = Karatsuba_Mult(a+b,c+d)
+	r = k - ac -bd
+	
+	return (10**n * ac) + (10**m * k) + bd
